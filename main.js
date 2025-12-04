b5ee798b38b36f58831780b1020e51d8f4b8ba1b
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-04 10:06:48 +0545

    Initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let operatorSymbol = "+";
+let num1 = 6;
+let num2 = 9;
+
+function operate(operatorSymbol, num1, num2) {
+  switch (operatorSymbol) {
+    case "+":
+      return add(num1, num2);
+      break;
+  }
+}
+// alert(operate(operatorSymbol, num1, num2));
+
+const calculatorDisplay = document.querySelector("#calculatorDisplay");
+const buttons = document.querySelectorAll("button");
+const buttonsArr = Array.from(buttons);
+buttonsArr.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (!isNaN(value)) {
+      if (calculatorDisplay.value === "0") {
+        calculatorDisplay.value = "";
+      } else {
+        calculatorDisplay.value += value;
+        return;
+      }
+    }
+    if (value === "AC") {
+      calculatorDisplay.value = "";
+      return;
+    }
+  });
+});
