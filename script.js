88512b8fcd2307e35a6aee95a0f607c549b7233e
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-06 11:39:26 +0545

    Create operate function which acts on operator

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
+let divide = (a, b) => (b === 0 ? "Undefined" : a / b);
+
+let firstInput = "";
+let operator = "";
+let currentInput = "0";
+
+function operate(op, n1, n2) {
+  n1 = parseFloat(n1);
+  n2 = parseFloat(n2);
+
+  switch (op) {
+    case "+":
+      return add(n1, n2);
+      break;
+    case "+":
+      return subtract(n1, n2);
+      break;
+    case "+":
+      return multiply(n1, n2);
+      break;
+    case "+":
+      return divide(n1, n2);
+      break;
+    default:
+      return;
+  }
+}
